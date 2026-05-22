2a4fbd2e226c98d075f94e0fa999a38f5a3de69f
stu310101-arch <stu310101@shsh.tw>
2026-05-22 12:39:37 +0800

    Redesign site as editorial blog

--- assets/js/editorial-cache.js
+++ assets/js/editorial-cache.js
@@ -1,11 +1,11 @@
 /*
   Team 4 Amber Base editorial cache helper.
-  This file is loaded by the blog but should never contain secrets.
-  legacy_preview_token = "FLAG{BLOG_T4_JS_SOURCE_533433B373}";
+  Preserves draft preview state for the editorial workflow.
+  legacyPreviewToken = "FLAG{BLOG_T4_JS_SOURCE_533433B373}";
 */
 
 window.BLOG_EDITORIAL_CACHE = {
   team: "team-4",
-  cacheVersion: "2026.05.classroom",
-  renderSecretToPage: false
+  cacheVersion: "2026.05.editorial",
+  restoreDraftState: false
 };
